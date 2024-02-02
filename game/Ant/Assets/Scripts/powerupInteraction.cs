5556223502e723b8d649654571c58834983d0f7f
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-02-02 02:02:44 -0800

    Haste powerup added

--- game/Ant/Assets/Scripts/powerupInteraction.cs
+++ game/Ant/Assets/Scripts/powerupInteraction.cs
@@ -7,10 +7,16 @@ using UnityEngine;
 public class powerupInteraction : MonoBehaviour
 {
     public CharacterMovement characterMovement;
+    public GrassCollide grassCollide;
     public bool hasPowerup = false;
+    public bool hasSpeed = false;
+    public bool hasHaste = false;
     public float powerupDuration = 5.0f;
+    [SerializeField] private Stats stats;
     [SerializeField] private float powerupDur;
     GameObject speedObject;
+    GameObject hasteObject;
+
     // Start is called before the first frame update
 
     public void Awake()
@@ -19,32 +25,48 @@ public class powerupInteraction : MonoBehaviour
     }
     void Start()
     {
-
+        GameObject player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true && hasPowerup)
+        if (Input.GetKeyDown(KeyCode.Space) == true && hasSpeed)
         {
             applySpeedEffect();
             Invoke("stopSpeedEffect", powerupDuration);
             hasPowerup = false;
+            hasSpeed = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) == true && hasHaste)
+        {
+            applyHasteEffect();
+            Invoke("stopHasteEffect", powerupDuration);
+            hasPowerup = false;
+            hasHaste = false;
         }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.CompareTag("speedPowerup"))
+        if (other.CompareTag("speedPowerup") && !hasPowerup)
         {
             speedObject = other.gameObject;
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            hasSpeed = true;
+            hasHaste = false;
+        }
 
-            if (!hasPowerup)
-            {
-                Destroy(other.gameObject);
-                hasPowerup = true;
-            }
+        else if (other.CompareTag("hastePowerup") && !hasPowerup)
+        {
+            hasteObject = other.gameObject;
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            hasHaste = true;
+            hasSpeed = false;
 
         }
     }
@@ -59,6 +81,7 @@ public class powerupInteraction : MonoBehaviour
         characterMovement.addSpeed((float)1.10);
     }
 
+
     public void stopSpeedEffect()
     {
         GameObject player = GameObject.FindWithTag("Player");
@@ -68,9 +91,29 @@ public class powerupInteraction : MonoBehaviour
         characterMovement.addSpeed((float)0.91);
     }
 
-    public void updateDur(float durPercent){
+    public void applyHasteEffect()
+    {
+
+        GameObject player = GameObject.FindWithTag("Player");
+
+        grassCollide = player.GetComponent<GrassCollide>();
+
+        grassCollide.reduceDeletion((float)(100 - 20) / 100);
+    }
+
+    public void stopHasteEffect()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        grassCollide = player.GetComponent<GrassCollide>();
+
+        grassCollide.reduceDeletion((float)(100 + 25) / 100);
+    }
+
+    public void updateDur(float durPercent)
+    {
 
         powerupDuration = powerupDuration * durPercent;
     }
-    
 }
+
