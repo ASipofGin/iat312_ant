a4553414fd2cd80134cb551aa6eb146c00476fac
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-02-02 14:45:54 -0800

    added mud and fixed ant hitbox

---
+++ game/Ant/Assets/obstacleInteraction.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleInteraction : MonoBehaviour
+{
+    public CharacterMovement characterMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Mud"))
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+
+            characterMovement = player.GetComponent<CharacterMovement>();
+            characterMovement.addSpeed((float)0.5f);
+
+
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Mud"))
+        {
+            Invoke("stopSlow", 1f); ;
+
+
+        }
+    }
+
+    public void stopSlow()
+    {
+
+        GameObject player = GameObject.FindWithTag("Player");
+
+        characterMovement = player.GetComponent<CharacterMovement>();
+
+        characterMovement.addSpeed((float)2);
+    }
+}
